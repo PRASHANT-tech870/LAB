19c1e39a74cf97a33a1325e4814d97c6942cfbeb
PRASHANT-tech870 <56446798+PRASHANT-tech870@users.noreply.github.com>
2024-08-14 11:11:04 +0530

    Update nQueens.c

--- DAA/nQueens.c
+++ DAA/nQueens.c
@@ -1,58 +1,67 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-int x[10];
-static int c = 1;
-void printBoard(int n)
-{
 
-    int i, j;
-    printf("Solution %d:\n", c++);
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= n; j++)
-        {
-            if (x[i] == j)
-                printf("Q\t");
+void printSolution(int board[][10], int N)
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (board[i][j])
+                printf("Q ");
             else
-                printf("-\t");
+                printf(". ");
         }
         printf("\n");
     }
+    printf("\n"); // Print a newline between solutions
 }
-int place(int k, int i)
-{
-    int j;
-    for (j = 1; j < k; j++)
-    {
-        if ((x[j] == i) || abs(x[j] - i) == abs(j - k))
-            return 0;
-    }
-    return 1;
-}
-void nQueens(int k, int n)
+
+bool isSafe(int board[][10], int row, int col, int N)
 {
     int i, j;
-    for (i = 1; i <= n; i++)
-    {
-        if (place(k, i))
-        {
-            x[k] = i;
-            if (k == n)
-            {
-                printf("\n");
-                printBoard(n);
-            }
-            else
-                nQueens(k + 1, n);
+
+    for (i = 0; i < col; i++)
+        if (board[row][i])
+            return false;
+
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j])
+            return false;
+
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+        if (board[i][j])
+            return false;
+
+    return true;
+}
+
+void solveNQUtil(int board[][10], int col, int N)
+{
+    if (col >= N) {
+        printSolution(board, N);
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (isSafe(board, i, col, N)) {
+            board[i][col] = 1;
+            solveNQUtil(board, col + 1, N);
+            board[i][col] = 0; 
         }
     }
 }
+
+void solveNQ(int N)
+{
+    int board[10][10] = { { 0 } };
+    solveNQUtil(board, 0, N);
+}
+
 int main()
 {
-    int n;
-    printf("Enter the number of queens:\n");
-    scanf("%d", &n);
-    nQueens(1, n);
-    if (c == 1)
-        printf("No solutions!");
+    int N;
+    printf("Enter the number of queens (N): ");
+    scanf("%d", &N);
+    solveNQ(N);
+    return 0;
 }
