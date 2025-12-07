b5895e12db37629900a906cc7f3d2bb92d442909
Prashant Ronad <56446798+PRASHANT-tech870@users.noreply.github.com>
2025-12-07 17:42:13 +0000

    Add matrix multiplication implementation with OpenMP
    This file implements matrix multiplication using OpenMP for parallel processing.

---
+++ PAGP/2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <omp.h>
+
+#define DIM 1000
+
+using namespace std;
+
+int main() {
+	int *A = new int[DIM * DIM];
+	int *B = new int[DIM * DIM];
+	int *C = new int[DIM * DIM];
+
+	double t = omp_get_wtime();
+
+	#pragma omp parallel for num_threads(1)
+	for (int i = 0; i < DIM; i++)
+		for (int j = 0; j < DIM; j++)
+			for (int k = 0; k < DIM; k++)
+				// C[i][k] -> C[DIM*i + k]
+				C[DIM*i + k] += A[DIM*i + j] * B[DIM*j + k];
+
+	t = omp_get_wtime() - t;
+	cout << "Time: " << t << endl;
+
+	delete[] A;
+	delete[] B;
+	delete[] C;
+}
+
+// g++ 2.cpp -fopenmp
