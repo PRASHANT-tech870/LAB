b4bbee39b1a04f3e664ef96bfb4e04e83560a654
Prashant Ronad <56446798+PRASHANT-tech870@users.noreply.github.com>
2025-12-07 18:03:30 +0000

    Create 5.c

---
+++ PAGP/5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+
+#define SIZE 4
+#define BUFSIZE 32
+
+#define ROOT 0
+#define TAG 0
+
+char* m[SIZE] = { "", "Hello", "RVCE", "CSE" };
+char msg[BUFSIZE];
+
+int main(int argc, char *argv[]) {
+	MPI_Init(&argc, &argv);
+
+	int rank, size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (size != SIZE) {
+		printf("Provide argument: -np %d\n", SIZE);
+		exit(1);
+	}
+
+	if (rank != ROOT) {
+		strcpy(msg, m[rank]);
+		MPI_Send(&msg, BUFSIZE, MPI_CHAR, ROOT, TAG, MPI_COMM_WORLD);
+	} else {
+		for(int i = 0; i < size; i++) {
+			if (i == ROOT) continue;
+			MPI_Recv(&msg, BUFSIZE, MPI_CHAR, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			printf("Received %s in process %d from process %d\n", msg, rank, i);
+		}
+	}
+
+	MPI_Finalize();
+}
+
+//sudo apt install mpich
+//mpicc -o mpi_example 5.c
+//mpirun -np 4 ./mpi_example
